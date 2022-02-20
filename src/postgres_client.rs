4580e25aba9c4e64d7d9ea4689e7df222f5f0cb8
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2022-02-19 17:42:32 -0800

    Do bulk inserts for indexes

--- src/postgres_client.rs
+++ src/postgres_client.rs
@@ -44,6 +44,7 @@ const DEFAULT_POSTGRES_PORT: u16 = 5432;
 const DEFAULT_THREADS_COUNT: usize = 100;
 const DEFAULT_ACCOUNTS_INSERT_BATCH_SIZE: usize = 10;
 const ACCOUNT_COLUMN_COUNT: usize = 9;
+const TOKEN_IDX_COLUMN_COUNT: usize = 2;
 const DEFAULT_PANIC_ON_DB_ERROR: bool = false;
 const DEFAULT_STORE_ACCOUNT_HISTORICAL_DATA: bool = false;
 
@@ -58,11 +59,23 @@ struct PostgresSqlClientWrapper {
     insert_account_audit_stmt: Option<Statement>,
     insert_token_owner_idx_stmt: Option<Statement>,
     insert_token_mint_idx_stmt: Option<Statement>,
+    bulk_insert_token_owner_idx_stmt: Option<Statement>,
+    bulk_insert_token_mint_idx_stmt: Option<Statement>,
+}
+
+/// Model the secondary index for token owner and mint
+pub struct TokenSecondaryIndex {
+    owner: Vec<u8>,
+    inner_key: Vec<u8>,
 }
 
 pub struct SimplePostgresClient {
     batch_size: usize,
     pending_account_updates: Vec<DbAccountInfo>,
+    index_token_owner: bool,
+    index_token_mint: bool,
+    pending_token_owner_idx: Vec<TokenSecondaryIndex>,
+    pending_token_mint_idx: Vec<TokenSecondaryIndex>,
     client: Mutex<PostgresSqlClientWrapper>,
 }
 
@@ -315,6 +328,86 @@ impl SimplePostgresClient {
                 )))
             }
             Ok(client) => Ok(client),
+        }
+    }
+
+    fn build_bulk_token_owner_index_insert_statement(
+        client: &mut Client,
+        config: &AccountsDbPluginPostgresConfig,
+    ) -> Result<Statement, AccountsDbPluginError> {
+        let batch_size = config
+            .batch_size
+            .unwrap_or(DEFAULT_ACCOUNTS_INSERT_BATCH_SIZE);
+        let mut stmt =
+            String::from("INSERT INTO spl_token_owner_index AS idx (owner_key, inner_key) VALUES");
+        for j in 0..batch_size {
+            let row = j * ACCOUNT_COLUMN_COUNT;
+            let val_str = format!("(${}, ${})", row + 1, row + 2,);
+
+            if j == 0 {
+                stmt = format!("{} {}", &stmt, val_str);
+            } else {
+                stmt = format!("{}, {}", &stmt, val_str);
+            }
+        }
+
+        let handle_conflict = "ON CONFLICT DO NOTHING";
+
+        stmt = format!("{} {}", stmt, handle_conflict);
+
+        info!("{}", stmt);
+        let bulk_stmt = client.prepare(&stmt);
+
+        match bulk_stmt {
+            Err(err) => {
+                return Err(AccountsDbPluginError::Custom(Box::new(AccountsDbPluginPostgresError::DataSchemaError {
+                    msg: format!(
+                        "Error in preparing for the token owner index update PostgreSQL database: {} host: {:?} user: {:?} config: {:?}",
+                        err, config.host, config.user, config
+                    ),
+                })));
+            }
+            Ok(update_account_stmt) => Ok(update_account_stmt),
+        }
+    }
+
+    fn build_bulk_token_mint_index_insert_statement(
+        client: &mut Client,
+        config: &AccountsDbPluginPostgresConfig,
+    ) -> Result<Statement, AccountsDbPluginError> {
+        let batch_size = config
+            .batch_size
+            .unwrap_or(DEFAULT_ACCOUNTS_INSERT_BATCH_SIZE);
+        let mut stmt =
+            String::from("INSERT INTO spl_token_mint_index AS idx (mint_key, inner_key) VALUES");
+        for j in 0..batch_size {
+            let row = j * ACCOUNT_COLUMN_COUNT;
+            let val_str = format!("(${}, ${})", row + 1, row + 2,);
+
+            if j == 0 {
+                stmt = format!("{} {}", &stmt, val_str);
+            } else {
+                stmt = format!("{}, {}", &stmt, val_str);
+            }
+        }
+
+        let handle_conflict = "ON CONFLICT DO NOTHING";
+
+        stmt = format!("{} {}", stmt, handle_conflict);
+
+        info!("{}", stmt);
+        let bulk_stmt = client.prepare(&stmt);
+
+        match bulk_stmt {
+            Err(err) => {
+                return Err(AccountsDbPluginError::Custom(Box::new(AccountsDbPluginPostgresError::DataSchemaError {
+                    msg: format!(
+                        "Error in preparing for the token mint index update PostgreSQL database: {} host: {:?} user: {:?} config: {:?}",
+                        err, config.host, config.user, config
+                    ),
+                })));
+            }
+            Ok(update_account_stmt) => Ok(update_account_stmt),
         }
     }
 
@@ -691,13 +784,79 @@ impl SimplePostgresClient {
         Ok(())
     }
 
+    fn queue_token_owner_idx_gen<G: GenericTokenAccount>(
+        &mut self,
+        token_id: &Pubkey,
+        account: &DbAccountInfo,
+    ) {
+        if account.owner() == token_id.to_bytes() {
+            if let Some(owner_key) = G::unpack_account_owner(account.data()) {
+                let owner_key = owner_key.to_bytes().to_vec();
+                let pubkey = account.pubkey();
+                self.pending_token_owner_idx.push(TokenSecondaryIndex {
+                    owner: owner_key,
+                    inner_key: pubkey.to_vec(),
+                });
+            }
+        }
+    }
+
+    fn queue_token_mint_idx_gen<G: GenericTokenAccount>(
+        &mut self,
+        token_id: &Pubkey,
+        account: &DbAccountInfo,
+    ) {
+        if account.owner() == token_id.to_bytes() {
+            if let Some(mint_key) = G::unpack_account_mint(account.data()) {
+                let mint_key = mint_key.to_bytes().to_vec();
+                let pubkey = account.pubkey();
+                self.pending_token_mint_idx.push(TokenSecondaryIndex {
+                    owner: mint_key,
+                    inner_key: pubkey.to_vec(),
+                })
+            }
+        }
+    }
+
+    /// Bulk insert secondary indexes
+    fn queue_secondary_indexes(&mut self, account: &DbAccountInfo) {
+        if self.index_token_owner {
+            self.queue_token_owner_idx_gen::<inline_spl_token::Account>(
+                &inline_spl_token::id(),
+                account,
+            );
+            self.queue_token_owner_idx_gen::<inline_spl_token_2022::Account>(
+                &inline_spl_token_2022::id(),
+                account,
+            );
+        }
+
+        if self.index_token_mint {
+            self.queue_token_mint_idx_gen::<inline_spl_token::Account>(
+                &inline_spl_token::id(),
+                account,
+            );
+            self.queue_token_mint_idx_gen::<inline_spl_token_2022::Account>(
+                &inline_spl_token_2022::id(),
+                account,
+            );
+        }
+    }
+
     /// Insert accounts in batch to reduce network overhead
     fn insert_accounts_in_batch(
         &mut self,
         account: DbAccountInfo,
     ) -> Result<(), AccountsDbPluginError> {
+        self.queue_secondary_indexes(&account);
         self.pending_account_updates.push(account);
 
+        self.bulk_insert_accounts()?;
+        self.bulk_insert_token_owner_idx()?;
+        self.bulk_insert_token_mint_idx()
+    }
+
+    fn bulk_insert_accounts(&mut self) -> Result<(), AccountsDbPluginError> {
         if self.pending_account_updates.len() == self.batch_size {
             let mut measure = Measure::start("accountsdb-plugin-postgres-prepare-values");
 
@@ -732,6 +891,7 @@ impl SimplePostgresClient {
                 .query(&client.bulk_account_insert_stmt, &values);
 
             self.pending_account_updates.clear();
+
             if let Err(err) = result {
                 let msg = format!(
                     "Failed to persist the update of account to the PostgreSQL database. Error: {:?}",
@@ -740,6 +900,7 @@ impl SimplePostgresClient {
                 error!("{}", msg);
                 return Err(AccountsDbPluginError::AccountsUpdateError { msg });
             }
+
             measure.stop();
             inc_new_counter_debug!(
                 "accountsdb-plugin-postgres-update-account-us",
@@ -749,6 +910,114 @@ impl SimplePostgresClient {
             );
             inc_new_counter_debug!(
                 "accountsdb-plugin-postgres-update-account-count",
+                self.batch_size,
+                10000,
+                10000
+            );
+        }
+        Ok(())
+    }
+
+    fn bulk_insert_token_owner_idx(&mut self) -> Result<(), AccountsDbPluginError> {
+        if self.pending_token_owner_idx.len() == self.batch_size {
+            let mut measure = Measure::start("accountsdb-plugin-postgres-prepare-owner-idx-values");
+
+            let mut values: Vec<&(dyn types::ToSql + Sync)> =
+                Vec::with_capacity(self.batch_size * TOKEN_IDX_COLUMN_COUNT);
+            for j in 0..self.batch_size {
+                let index = &self.pending_token_owner_idx[j];
+                values.push(&index.owner);
+                values.push(&index.inner_key);
+            }
+            measure.stop();
+            inc_new_counter_debug!(
+                "accountsdb-plugin-postgres-prepare-owner-idx-values-us",
+                measure.as_us() as usize,
+                10000,
+                10000
+            );
+
+            let mut measure = Measure::start("accountsdb-plugin-postgres-update-owner-idx-account");
+            let client = self.client.get_mut().unwrap();
+            let result = client.client.query(
+                client.bulk_insert_token_owner_idx_stmt.as_ref().unwrap(),
+                &values,
+            );
+
+            self.pending_token_owner_idx.clear();
+
+            if let Err(err) = result {
+                let msg = format!(
+                    "Failed to persist the update of account to the PostgreSQL database. Error: {:?}",
+                    err
+                );
+                error!("{}", msg);
+                return Err(AccountsDbPluginError::AccountsUpdateError { msg });
+            }
+
+            measure.stop();
+            inc_new_counter_debug!(
+                "accountsdb-plugin-postgres-update-owner-idx-us",
+                measure.as_us() as usize,
+                10000,
+                10000
+            );
+            inc_new_counter_debug!(
+                "accountsdb-plugin-postgres-update-owner-idx-count",
+                self.batch_size,
+                10000,
+                10000
+            );
+        }
+        Ok(())
+    }
+
+    fn bulk_insert_token_mint_idx(&mut self) -> Result<(), AccountsDbPluginError> {
+        if self.pending_token_owner_idx.len() == self.batch_size {
+            let mut measure = Measure::start("accountsdb-plugin-postgres-prepare-mint-idx-values");
+
+            let mut values: Vec<&(dyn types::ToSql + Sync)> =
+                Vec::with_capacity(self.batch_size * TOKEN_IDX_COLUMN_COUNT);
+            for j in 0..self.batch_size {
+                let index = &self.pending_token_owner_idx[j];
+                values.push(&index.owner);
+                values.push(&index.inner_key);
+            }
+            measure.stop();
+            inc_new_counter_debug!(
+                "accountsdb-plugin-postgres-prepare-mint-idx-values-us",
+                measure.as_us() as usize,
+                10000,
+                10000
+            );
+
+            let mut measure = Measure::start("accountsdb-plugin-postgres-update-mint-idx-account");
+            let client = self.client.get_mut().unwrap();
+            let result = client.client.query(
+                client.bulk_insert_token_mint_idx_stmt.as_ref().unwrap(),
+                &values,
+            );
+
+            self.pending_token_owner_idx.clear();
+
+            if let Err(err) = result {
+                let msg = format!(
+                    "Failed to persist the update of account to the PostgreSQL database. Error: {:?}",
+                    err
+                );
+                error!("{}", msg);
+                return Err(AccountsDbPluginError::AccountsUpdateError { msg });
+            }
+
+            measure.stop();
+            inc_new_counter_debug!(
+                "accountsdb-plugin-postgres-update-mint-idx-us",
+                measure.as_us() as usize,
+                10000,
+                10000
+            );
+            inc_new_counter_debug!(
+                "accountsdb-plugin-postgres-update-mint-idx-count",
                 self.batch_size,
                 10000,
                 10000
@@ -806,6 +1075,20 @@ impl SimplePostgresClient {
             None
         };
 
+        let bulk_insert_token_owner_idx_stmt = if let Some(true) = config.index_token_owner {
+            let stmt = Self::build_bulk_token_owner_index_insert_statement(&mut client, config)?;
+            Some(stmt)
+        } else {
+            None
+        };
+
+        let bulk_insert_token_mint_idx_stmt = if let Some(true) = config.index_token_mint {
+            let stmt = Self::build_bulk_token_mint_index_insert_statement(&mut client, config)?;
+            Some(stmt)
+        } else {
+            None
+        };
+
         let insert_token_owner_idx_stmt = if let Some(true) = config.index_token_owner {
             Some(Self::build_single_token_owner_index_upsert_statement(
                 &mut client,
@@ -839,7 +1122,13 @@ impl SimplePostgresClient {
                 insert_account_audit_stmt,
                 insert_token_owner_idx_stmt,
                 insert_token_mint_idx_stmt,
+                bulk_insert_token_owner_idx_stmt,
+                bulk_insert_token_mint_idx_stmt,
             }),
+            index_token_owner: config.index_token_owner.unwrap_or_default(),
+            index_token_mint: config.index_token_mint.unwrap_or(false),
+            pending_token_owner_idx: Vec::with_capacity(batch_size),
+            pending_token_mint_idx: Vec::with_capacity(batch_size),
         })
     }
 }
