7732ad7808e0aefe6040542a068d8af5eb7ce024
skynetcap <100323448+skynetcap@users.noreply.github.com>
2022-09-30 11:14:08 -0700

    Use same logic for V2 transactions.

--- src/geyser_plugin_postgres.rs
+++ src/geyser_plugin_postgres.rs
@@ -372,7 +372,27 @@ impl GeyserPlugin for GeyserPluginPostgres {
                 )));
             }
             Some(client) => match transaction_info {
-                ReplicaTransactionInfoVersions::V0_0_1(transaction_info) | ReplicaTransactionInfoVersions::V0_0_2(transaction_info) => {
+                ReplicaTransactionInfoVersions::V0_0_1(transaction_info) => {
+                    if let Some(transaction_selector) = &self.transaction_selector {
+                        if !transaction_selector.is_transaction_selected(
+                            transaction_info.is_vote,
+                            Box::new(transaction_info.transaction.message().account_keys().iter()),
+                        ) {
+                            return Ok(());
+                        }
+                    } else {
+                        return Ok(());
+                    }
+
+                    let result = client.log_transaction_info(transaction_info, slot);
+
+                    if let Err(err) = result {
+                        return Err(GeyserPluginError::SlotStatusUpdateError{
+                                msg: format!("Failed to persist the transaction info to the PostgreSQL database. Error: {:?}", err)
+                            });
+                    }
+                },
+                ReplicaTransactionInfoVersions::V0_0_2(transaction_info) => {
                     if let Some(transaction_selector) = &self.transaction_selector {
                         if !transaction_selector.is_transaction_selected(
                             transaction_info.is_vote,
