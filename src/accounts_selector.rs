e71e83cd68914f44a40f32c4cf62383d0638f38a
0xward <aradeawardana97@gmail.com>
2026-05-14 05:47:36 +0700

    feat: enhance protocol logic in accounts_selector.rs

--- src/accounts_selector.rs
+++ src/accounts_selector.rs
@@ -1,21 +1,23 @@
-use {log::*, std::collections::HashSet};
+use {log::*, solana_sdk::pubkey::Pubkey, std::collections::HashSet, std::str::FromStr};
 
 #[derive(Debug)]
 pub(crate) struct AccountsSelector {
-    pub accounts: HashSet<Vec<u8>>,
-    pub owners: HashSet<Vec<u8>>,
+    pub accounts: HashSet<Pubkey>,
+    pub owners: HashSet<Pubkey>,
     pub select_all_accounts: bool,
 }
 
-impl AccountsSelector {
-    pub fn default() -> Self {
+impl Default for AccountsSelector {
+    fn default() -> Self {
         AccountsSelector {
             accounts: HashSet::default(),
             owners: HashSet::default(),
             select_all_accounts: true,
         }
     }
+}
 
+impl AccountsSelector {
     pub fn new(accounts: &[String], owners: &[String]) -> Self {
         info!(
             "Creating AccountsSelector from accounts: {:?}, owners: {:?}",
@@ -30,14 +32,17 @@ impl AccountsSelector {
                 select_all_accounts,
             };
         }
+
         let accounts = accounts
             .iter()
-            .map(|key| bs58::decode(key).into_vec().unwrap())
+            .map(|key| Pubkey::from_str(key).unwrap_or_else(|_| panic!("Invalid account pubkey: {}", key)))
             .collect();
+
         let owners = owners
             .iter()
-            .map(|key| bs58::decode(key).into_vec().unwrap())
+            .map(|key| Pubkey::from_str(key).unwrap_or_else(|_| panic!("Invalid owner pubkey: {}", key)))
             .collect();
+
         AccountsSelector {
             accounts,
             owners,
@@ -45,11 +50,11 @@ impl AccountsSelector {
         }
     }
 
-    pub fn is_account_selected(&self, account: &[u8], owner: &[u8]) -> bool {
+    pub fn is_account_selected(&self, account: &Pubkey, owner: &Pubkey) -> bool {
         self.select_all_accounts || self.accounts.contains(account) || self.owners.contains(owner)
     }
 
-    /// Check if any account is of interested at all
+    /// Check if any account is of interest at all
     pub fn is_enabled(&self) -> bool {
         self.select_all_accounts || !self.accounts.is_empty() || !self.owners.is_empty()
     }
